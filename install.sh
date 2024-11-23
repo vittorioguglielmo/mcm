335718ff93d8d39ba51a4b678904a27d0ea95e67
Vittorio Guglielmo <vittorio.guglielmo@gmail.com>
2024-11-23 18:46:56 +0100

    .

--- install.sh
+++ install.sh
@@ -13,11 +13,18 @@ apikey=$user_full_name
 cluster="mi"
 akext=${user_name:(-4)}
 log_local_prefix=$cluster$akext
+dnow=$(date +%Y-%m-%d-%H:%M)
+installation_log="installation-$dnow.log"
+
+
+if [ -f "$installation_log" ] ; then
+	rm $installation_log
+fi
 
 installa_log() {
 [[ -d /home/$user_name/archived_logs ]] || sudo mkdir /home/$user_name/archived_logs
 if [ ! -f etc/logrotate_"$user_name"_conf ]; then
-wget https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/logrotate_USERNAME_conf -O logrotate_USERNAME_conf
+wget -q https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/logrotate_USERNAME_conf -O logrotate_USERNAME_conf
 sudo cp logrotate_USERNAME_conf /etc/logrotate_"$user_name"_conf
 sudo sed -i "s/USERNAME/$user_name/g" /etc/logrotate_"$user_name"_conf
 sudo sed -i "s/LOCALPREFIX/$log_local_prefix/g" /etc/logrotate_"$user_name"_conf
@@ -26,20 +33,45 @@ fi
 
 
 installa_crontab() {
+echo
+echo
 line="*/1 * * * * /usr/sbin/logrotate /etc/logrotate_"$user_name"_conf"
 if ! sudo crontab -l | grep -q "/etc/logrotate_"$user_name"_conf" ;then
 (sudo crontab  -l; echo "$line" ) | sudo crontab  -
 fi
+
+if [ $? -eq 0 ]; then
+   printf '%b\n' "Crontab installato: ${green} OK ${clear}"
+   echo "Crontab installato con successo: OK" >> $installation_log
+   echo >> $installation_log
+   fi
+
 }
 
 installa_miner_desktop() {
-wget -O Miner.desktop https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/Miner.desktop
+echo
+echo
+wget -q -O Miner.desktop https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/Miner.desktop
 mv Miner.desktop /home/$user_name/.config/autostart
+if [ $? -eq 0 ]; then
+   printf '%b\n' "Partenza automatica miner: ${green} OK ${clear}"
+   echo "Partenza automatica miner installata con successo: OK" >> $installation_log
+   echo >> $installation_log
+   fi
+
 }
 
 installa_rustdesk_desktop() {
-wget -O Rustdesk.desktop https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/Rustdesk.desktop
+echo
+echo
+wget -q -O Rustdesk.desktop https://github.com/vittorioguglielmo/mcm/raw/refs/heads/main/Rustdesk.desktop
 mv Rustdesk.desktop //home/$user_name/.config/autostart
+if [ $? -eq 0 ]; then
+   printf '%b\n' "Partenza automatica Rustdesk: ${green} OK ${clear}"
+   echo "Partenza automatica Rustdesk installata con successo: OK" >> $installation_log
+   echo >> $installation_log
+   fi
+sleep 3
 }
 
 #SOSTITUISCO PLACEHOLDER con utente in sudoers e copio il file
@@ -48,6 +80,7 @@ sudo sh -c "echo '$user_name ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/dont-
 installa_rustdesk() {
 # Scarico il JSON contentente le info sull'ultima versione di Rustdesk presente sulla repository GitHub
 REQUEST=$(curl -s "https://api.github.com/repos/rustdesk/rustdesk/releases/latest")
+
 
 # Estraggo dal JSON il link per scaricare l'ultima versione di Rustdesk per Linux Debian x86_64
 DOWNLOAD_URL=$(echo "$REQUEST" | jq -r '.assets[] | select(.browser_download_url | contains("-x86_64.deb")) | .browser_download_url'|head -n 1)
@@ -65,6 +98,13 @@ else
     wget $DOWNLOAD_URL -O /tmp/rustdesk-x86_64.deb
     sudo apt install /tmp/rustdesk-x86_64.deb 
 fi
+if [ $? -eq 0 ]; then
+        printf '%b\n' "Installato Rustdesk: ${green} OK ${clear}"
+        echo "Installato Rustdesk : OK" >> $installation_log
+        echo >> $installation_log
+fi
+sleep 2
+main_menu
 }
 
 
@@ -80,8 +120,15 @@ if [ -d "/home/mcm${homesub}/RainbowMiner" ] ; then
         read -p "Sembra che il miner sia già stato installato, vuoi reinstallarlo? [S/N]" yn
     case $yn in
         [Ss]* ) sudo rm -rf /home/mcm${homesub}/RainbowMiner;
+		if [ $? -eq 0 ]; then
+                printf '%b\n' "Rimozione directory Rainbowminer: ${green} OK ${clear}"
+        	echo "Rimozione directory Rainbowminer: OK" >> $installation_log
+        	echo >> $installation_log
+      		fi
                 break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo "Uscita dal programma evitando la rimozione della directory di RainbowMiner: OK" >> $installation_log;
+		echo >> $installation_log;
+		exit;;
         * ) echo "Rispondi [s]i o [n]";;
     esac
     	else break
@@ -103,19 +150,21 @@ echo "Esecuzione di install_new_OS_mcm300.sh interrotta con CTRL+C"
 echo 
 echo "Finalizzo installazione con update_300.sh"
 echo
+sleep 2
+echo "Esecuzione di install_new_OS_mcm300.sh interrotta con CTRL+C" >> $installation_log
+echo >> $installation_log
+
 wget -O update_300.sh https://hashburst.io/nodes/rigs/update_300.sh
 sudo chmod +x update_300.sh
 sudo bash ./update_300.sh $apikey $cluster
 
-while true; do
-        read -p "Finalizzazione terminata, procedo con il reboot? [S/N]" yn
-    case $yn in
-        [Ss]* ) sudo reboot;
-                break;;
-        [Nn]* ) exit;;
-        * ) echo "Rispondi [s]i o [n]";;
-    esac
-done
+if [ $? -eq 0 ]; then
+   printf '%b\n' "Update_300.sh completato con successo: ${green} OK ${clear}"
+   sleep 2
+   echo "Update_300.sh completato con successo: OK" >> $installation_log
+   echo >> $installation_log
+   fi
+
 
 unset finalizing_miner_installation
 trap "$trap_sigint" SIGINT
@@ -126,7 +175,21 @@ trap_sigint="$(trap -p SIGINT)"
 trap "finalizing_miner_installation" SIGINT SIGTERM
 script -c "sudo bash ./install_new_OS_mcm300.sh $apikey $cluster" output_miner_installation.txt
 finalizing_miner_installation
+main_menu
 }	
+
+reboot() {
+	
+while true; do
+        read -p "Finalizzazione terminata, procedo con il reboot? [S/N]" yn
+    case $yn in
+        [Ss]* ) sudo reboot;
+                break;;
+        [Nn]* ) exit;;
+        * ) echo "Rispondi [s]i o [n]";;
+    esac
+done
+}
 
 ####Controllo se ho accesso a Internet
 
@@ -147,8 +210,9 @@ start=$(whiptail --separate-output --title "Installazione MCM Miner" --default-i
   1 "Installa aggiornamenti" on \
   2 "Installa Rustdesk" off \
   3 "Installa il software di mining" off \
-  4 "Installa add-on (log-rotation e crontab e altro)" off \
-  5 "Esci" off \
+  4 "Installa add-on (log-rotation, crontab e automatismi)" off \
+  5 "Reboot" off \
+  6 "Esci" off \
   3>&1 1>&2 2>&3)
 
 if [ -z "$start" ]; then
@@ -158,7 +222,12 @@ else
     case "$CHOICE" in
     "1")
       clear && sudo apt update -y && sudo apt upgrade -y
-      #sudo apt install -y rustdesk
+      if [ $? -eq 0 ]; then
+    	printf '%b\n' "Aggiornato il sistema operativo: ${green} OK ${clear}"
+	echo "Aggiornato sistema operativo: OK" >> $installation_log
+	echo >> $installation_log
+      fi
+      sleep 2 && main_menu
       ;;
     "2")
       installa_rustdesk
@@ -171,10 +240,17 @@ else
       installa_log;
       installa_miner_desktop;
       installa_rustdesk_desktop
+      main_menu
+      ;;
+    "5")
+      reboot
+      ;;
+    "6")
+      exit 0
       ;;
     *)
-      echo "Unsupported item $CHOICE!" >&2
-      exit 1
+      echo "Scelta non supportata: $CHOICE!" >&2
+      main_menu
       ;;
     esac
   done
